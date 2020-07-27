a9554f7213ed38d75d1ad22d6655b816de68052b
hj <a301dks@naver.com>
2020-07-27 13:23:52 +0900

    ㅌㅓ지는거 테스트

--- src/test/java/wooteco/subway/admin/SubwayAdminApplicationTests.java
+++ src/test/java/wooteco/subway/admin/SubwayAdminApplicationTests.java
@@ -1,5 +1,7 @@
 package wooteco.subway.admin;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class SubwayAdminApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(0);
 	}
 
 }
